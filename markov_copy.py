ceea22a4d6bcd51aff795faede96df30dd670a18
Hackbright Student <info@hackbrightacademy.com>
2015-07-08 13:06:16 -0700

    working random text, markov

--- markov_copy.py
+++ markov_copy.py
@@ -1,7 +1,8 @@
 import random
 
-opened_file = open("green-eggs.txt")
-def make_chains(corpus):
+working_filename = "green-eggs.txt"
+def make_chains(working_filename):
+    corpus = open(working_filename)
     text = corpus.read()
     """Takes input text as string; returns dictionary of markov chains."""
     random_dict = {}
@@ -27,14 +28,38 @@ def make_chains(corpus):
         i += 1    
 
 
+    #print random_dict
+
+    corpus.close()
+
     return random_dict    
 
 
 
-def make_text(dict_chains):
+def make_text(chain_dict):
     """Takes dictionary of markov chains; returns random text."""
+    
+    # loop - while rand_key in chain_dict:
+    rand_key = random.choice(chain_dict.keys())
+    text_random = rand_key[0] + " " + rand_key[1]
+    while rand_key in chain_dict:
+        #string_key = rand_key[0] + " " + rand_key[1]
+        next_word_list = chain_dict[rand_key]
+        new_word = random.choice(next_word_list) 
 
-    random.choice(dict_chains.keys())
+        text_random = text_random + " " + new_word 
+
+        rand_key = (rand_key[1],new_word)
+
+    print text_random
+
+
+    # print rand_key, next_word_list
+
+    # print " "
+
+    # print string_key, new_word 
+
     
     
 
@@ -45,12 +70,12 @@ def make_text(dict_chains):
 # be used by examining the `sys.argv` arguments (if neccessary, see the
 # Python docs for sys.argv)
 
-make_chains(opened_file)
+make_chains(working_filename)
 
 #input_text = "Some text"
 
 # Get a Markov chain
-chain_dict = make_chains(opened_file)
+chain_dict = make_chains(working_filename)
 
 # Produce random text
 random_text = make_text(chain_dict)
